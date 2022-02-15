b511845f3c480994234710ab8102a9f7fb5a6e88
Abhinay <garg.abhinay8602@gmail.com>
2022-02-15 15:38:57 +0530

    Use of Hooks like UseEffect, useReducer, useRef, useContext and use useImperative Handler

---
+++ 10-advanced-handling-side-effects/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
+import AuthContext, { AuthContextProvider } from "./store/auth-context";
+
+ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
+  document.getElementById("root")
+);
