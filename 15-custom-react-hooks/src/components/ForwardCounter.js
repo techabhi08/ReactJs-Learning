8ca1c35bf0dfe654bb160dc2df35c13d92dd5059
techabhi08 <abhinaygarg8@hotmail.com>
2022-07-21 14:26:34 +0530

    section 15 part 1, building custom hooks

---
+++ 15-custom-react-hooks/src/components/ForwardCounter.js
@@ -0,0 +1,20 @@
+import useCounter from '../hooks/use-counter';
+
+import Card from './Card';
+
+const ForwardCounter = () => {
+  const counter = useCounter();
+  // const [counter, setCounter] = useState(0);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCounter((prevCounter) => prevCounter + 1);
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
