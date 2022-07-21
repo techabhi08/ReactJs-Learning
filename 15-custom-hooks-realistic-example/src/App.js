8c8cae25eab7c53ef8ee793d4d16439748a29e07
techabhi08 <garg.abhinay8602@gamil.com>
2022-07-21 17:43:01 +0530

    added 2nd part of mod15, learning custom hooks

---
+++ 15-custom-hooks-realistic-example/src/App.js
@@ -0,0 +1,42 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (taskObj) => {
+      const loadedTasks = [];
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    };
+    fetchTasks(    { url: "https://react-http-dc1f3-default-rtdb.firebaseio.com/tasks.json" }, transformTasks);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
