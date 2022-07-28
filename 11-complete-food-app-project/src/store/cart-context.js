f76f5de41b9b4fb3acde76d4465d789845802ab5
techabhi08 <garg.abhinay8602@gmail.com>
2022-07-28 14:38:30 +0530

    Added backend and form validation

--- 11-complete-food-app-project/src/store/cart-context.js
+++ 11-complete-food-app-project/src/store/cart-context.js
@@ -4,7 +4,8 @@ const cartContext = React.createContext({
     items: [],
     totalAmount: 0,
     addItem: (item) => {},
-    removeItem: (id) => {}
+    removeItem: (id) => {},
+    clearCart: () => {}
 });
 
 export default cartContext;
