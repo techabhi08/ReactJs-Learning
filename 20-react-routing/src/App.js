70a79c0dfb211d158fc6e03c6d893c741c010563
techabhi08 <garg.abhinay8602@gmail.com>
2022-08-10 20:12:20 +0530

    added learning of React routing added React routing part

---
+++ 20-react-routing/src/App.js
@@ -0,0 +1,34 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+        <Route path="/" exact>
+          <Redirect to='/welcome'/>
+        </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+//our-domain.com/welcome => Welcome Component
+//our-domain.com/products => Products Component
